499095606f91a3eaa508045b3bc5eb25e340cc79
ChineduEjiogu1 <chineduejiogu@gmail.com>
2020-02-23 02:06:47 -0500

    sqlite nodejs not inserting

--- passwords.sql
+++ passwords.sql
@@ -3,6 +3,57 @@ DROP TABLE IF EXISTS PASSWORDS;
 
 CREATE TABLE PASSWORDS(
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    USER_NAMES VARCHAR(30),
-    SALT_and_HASH_PASSWORD CHAR BINARY(60)
+    USER_NAME VARCHAR(30),
+    SALT_and_HASH_PASSWORD CHAR BINARY(60),
+    FOREIGN KEY(ID) REFERENCES USERS(uID)
 );
+
+INSERT INTO PASSWORDS(
+    USER_NAME,
+    SALT_and_HASH_PASSWORD   
+)
+values(
+        'nick11', '1ivorynewt97'
+    );
+
+-- INSERT INTO PASSWORDS(
+--     USER_NAME,
+--     SALT_and_HASH_PASSWORD   
+-- )
+-- values(
+--         'white00', 'coolspace87'
+--     );
+
+-- INSERT INTO PASSWORDS(
+--     USER_NAME,
+--     SALT_and_HASH_PASSWORD   
+-- )
+-- values(
+--         'kira_flo', 'jumpyplane62'
+--     );
+
+-- INSERT INTO PASSWORDS(
+--     USER_NAME,
+--     SALT_and_HASH_PASSWORD   
+-- )
+-- values(
+--         'cali_flo', 'mistymusk72'
+--     );
+
+-- INSERT INTO PASSWORDS(
+--     USER_NAME,
+--     SALT_and_HASH_PASSWORD   
+-- )
+-- values(
+--         'eli_easy', 'longsteel12'
+--     );
+
+
+-- INSERT INTO PASSWORDS(
+--     USER_NAME,
+--     SALT_and_HASH_PASSWORD   
+-- )
+-- values(
+--         'Sophia10', 'kindbone84'
+--     );
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+var sqlite3 = require('sqlite3');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res) {
+  res.send('login', {title:'login in'});
 });
+
+router.post('/', function(req, res){
+
+  const saltRounds = 10;
+  const plainTextPassword1 = req.body.pass;
+
+  bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+  
+    //let sql = 'INSERT INTO PASSWORDS (USER_NAME, SALT_and_HASH_PASSWORD) VALUES (?, ?)';
+
+      db.run(`INSERT INTO PASSWORDS (USER_NAME, SALT_and_HASH_PASSWORD) VALUES (?, ?)`, ['username, password'], 
+      function(err) {
+        if (err) {
+          console.log(err.message);
+        }
+        // get the last insert id
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
+
+      let db = new sqlite3.Database('demo.db', (err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log('Connected to password db.');
+      });
+    
+      db.close((err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log('Close the database connection.');
+      });
+    
+    db.close();
+    res.send("done!");
+  
+  });
+
+})
 
 module.exports = router;
